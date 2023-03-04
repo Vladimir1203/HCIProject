787e177b908b847defaafbc3a8b6aa8fbf0bfecc
Vladimir1203 <vladimir12934@gmail.com>
2023-03-05 00:14:05 +0100

    2 - footer and header components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { AppRoutingModule } from "./app.routing.module";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
